511acb8102b26ec0460f1d3c7ce21a9941f095ff
Cyril SIX <cyril.six@kalray.eu>
2018-04-18 14:03:48 +0200

    MPPA - added a Matrix Multiply test

---
+++ test/mppa/mmult/mmult.c
@@ -0,0 +1,54 @@
+#include "../lib/types.h"
+#include "../lib/prng.h"
+
+#ifdef __UNIT_TEST_MMULT__
+#define SIZE 50
+#else
+#include "test.h"
+#endif
+
+void mmult_row(uint64_t C[][SIZE], uint64_t A[][SIZE], uint64_t B[][SIZE]){
+    for (int i = 0 ; i < SIZE ; i++)
+        for (int j = 0 ; j < SIZE ; j++)
+            C[i][j] = 0;
+
+    for (int i = 0 ; i < SIZE ; i++)
+        for (int j = 0 ; j < SIZE ; j++)
+            for (int k = 0 ; k < SIZE ; k++)
+                C[i][j] += A[i][k] * B[k][j];
+}
+
+void mmult_col(uint64_t C[][SIZE], uint64_t A[][SIZE], uint64_t B[][SIZE]){
+    for (int i = 0 ; i < SIZE ; i++)
+        for (int j = 0 ; j < SIZE ; j++)
+            C[i][j] = 0;
+
+    for (int k = 0 ; k < SIZE ; k++)
+        for (int i = 0 ; i < SIZE ; i++)
+            for (int j = 0 ; j < SIZE ; j++)
+                C[i][j] += A[i][k] * B[k][j];
+}
+
+#ifdef __UNIT_TEST_MMULT__
+static uint64_t C1[SIZE][SIZE], C2[SIZE][SIZE], A[SIZE][SIZE], B[SIZE][SIZE];
+
+int main(void){
+    srand(42);
+
+    for (int i = 0 ; i < SIZE ; i++)
+        for (int j = 0 ; j < SIZE ; j++){
+            A[i][j] = randlong();
+            B[i][j] = randlong();
+        }
+
+    mmult_row(C1, A, B);
+    mmult_col(C2, A, B);
+
+    for (int i = 0 ; i < SIZE ; i++)
+        for (int j = 0 ; j < SIZE ; j++)
+            if (C1[i][j] != C2[i][j])
+                return -1;
+
+    return 0;
+}
+#endif /* __UNIT_TEST_MMULT__ */
---
+++ test/mppa/mmult/mmult.h
@@ -0,0 +1,9 @@
+#ifndef __MMULT_H__
+#define __MMULT_H__
+
+#include "../lib/types.h"
+
+void mmult_row(uint64_t *A, const uint64_t *B, const uint64_t *C);
+void mmult_column(uint64_t *A, const uint64_t *B, const uint64_t *C);
+
+#endif /* __MMULT_H__ */
