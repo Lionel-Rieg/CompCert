ea81391aa016d914d9fabfc209afddf230e65aa9
Cyril SIX <cyril.six@kalray.eu>
2019-01-08 11:28:46 +0100

    Reorganized PostpassOracle to separate asmblock instructions from real instructions

--- mppa_k1c/PostpassSchedulingOracle.ml
+++ mppa_k1c/PostpassSchedulingOracle.ml
@@ -1,8 +1,103 @@
 open Asmblock
 open Printf
 open Camlcoq
+(* open InstructionScheduler *)
 
-(** Resource functions *)
+(**
+ * Extracting infos from Asmblock instructions
+ *)
+
+type immediate = I32 of Integers.Int.int | I64 of Integers.Int64.int
+
+type ab_inst_rec = {
+  inst: string; (* name of the pseudo instruction *)
+  write_regs : gpreg list;
+  read_regs : gpreg list;
+  imm : immediate option;
+}
+
+let arith_rrr_rec i rd rs1 rs2 = match i with
+  | Paddl -> { inst="Paddl" ; write_regs = [rd]; read_regs = [rs1; rs2]; imm = None}
+  | _ -> failwith "arith_rrr_rec: unrecognized constructor"
+
+let arith_rri32_rec i rd rs imm32 = match i with
+  | Paddiw -> 
+      { inst = "Paddiw"; write_regs = [rd]; read_regs = [rs]; imm=imm32 }
+  | _ -> failwith "arith_rri32_rec: unrecognized constructor"
+
+let arith_rri64_rec i rd rs imm64 = match i with
+  | Paddil ->
+      { inst = "Paddil"; write_regs = [rd]; read_regs = [rs]; imm=imm64 }
+  | _ -> failwith "arith_rri64_rec: unrecognized constructor"
+
+let arith_rec i =
+  match i with
+  | PArithRRI32 (i, rd, rs, imm32) -> arith_rri32_rec i rd rs (Some (I32 imm32))
+  | PArithRRI64 (i, rd, rs, imm64) -> arith_rri64_rec i rd rs (Some (I64 imm64))
+  | PArithRRR (i, rd, rs1, rs2) -> arith_rrr_rec i rd rs1 rs2
+  | _ -> failwith "arith_rec: unrecognized constructor"
+
+let basic_rec i =
+  match i with
+  | PArith i -> arith_rec i
+  | _ -> failwith "basic_rec: unrecognized constructor"
+
+let control_rec i = failwith "control_rec: not implemented"
+
+let rec basic_recs body = match body with
+  | [] -> []
+  | bi :: body -> (basic_rec bi) :: (basic_recs body)
+
+let exit_rec exit = match exit with
+  | None -> []
+  | Some ex -> [control_rec ex]
+
+let instruction_recs bb = (basic_recs bb.body) @ (exit_rec bb.exit)
+
+(**
+ * Providing informations relative to the real instructions
+ *)
+
+(** Abstraction providing all the necessary informations for solving the scheduling problem *)
+type inst_info = {
+  write_regs : gpreg list;
+  read_regs : gpreg list;
+  usage: int array; (* resources consumed by the instruction *)
+}
+
+(** Figuring out whether an immediate is s10, u27l10 or e27u27l10 *)
+type imm_encoding = S10 | U27l10 | E27u27l10
+
+let rec pow a = function
+  | 0 -> 1
+  | 1 -> a
+  | n -> let b = pow a (n/2) in
+         b * b * (if n mod 2 = 0 then 1 else a)
+
+let signed_interval n = begin 
+  assert (n > 0);
+  let min = - pow 2 (n-1)
+  and max = pow 2 (n-1) - 1
+  in (min, max)
+end
+
+let within i interv = match interv with (min, max) -> (i >= min && i <= max)
+
+let signed_length i =
+  let rec f i n = 
+    let interv = signed_interval n
+    in if (within i interv) then n else f i (n+1)
+  in f i 0
+
+let encode_imm imm =
+  let i = Z.to_int imm
+  in let length = signed_length i
+  in if length <= 10 then S10
+  else if length <= 37 then U27l10
+  else if length <= 64 then E27u27l10
+  else failwith @@ sprintf "encode_imm: integer too big! (%d)" i
+
+(** Resources *)
 let resource_names = ["ISSUE"; "TINY"; "LITE"; "ALU"; "LSU"; "MAU"; "BCU"; "ACC"; "DATA"; "TCA"; "BRE"; "BRO"; "NOP"]
 
 let rec find_index elt l =
@@ -45,88 +140,32 @@ let alu_tiny_y : int array = let resmap = fun r -> match r with
   | "ISSUE" -> 3 | "TINY" -> 1 | _ -> 0 
   in Array.of_list (List.map resmap resource_names)
 
-(** Mapping instruction -> instruction info *)
+(** Real instructions *)
 
-type inst_info = {
-  reservation : int array;
-  write_regs : gpreg list;
-  read_regs : gpreg list;
-}
+type real_instruction = Addw | Addd
 
-(* Figuring out whether an immediate is s10, u27l10 or e27u27l10 *)
-type imm_encoding = S10 | U27l10 | E27u27l10
+let ab_inst_to_real = function
+  | "Paddl" | "Paddil" -> "addd"
+  | "Paddw" | "Paddiw" -> "addw"
+  | s -> failwith @@ sprintf "ab_inst_to_real: unrecognized instruction: %s" s
 
-let rec pow a = function
-  | 0 -> 1
-  | 1 -> a
-  | n -> let b = pow a (n/2) in
-         b * b * (if n mod 2 = 0 then 1 else a)
+let rec_to_usage r =
+  let encoding = match r.imm with None -> None | Some (I32 i) | Some (I64 i) -> Some (encode_imm i)
+  and real_inst = ab_inst_to_real r.inst
+  and fail i = failwith @@ sprintf "rec_to_usage: failed with instruction %s" i
+  in match real_inst with
+  | "addw" -> (match encoding with None | Some S10 -> alu_tiny | Some U27l10 -> alu_tiny_x | Some E27u27l10 -> fail real_inst)
+  | "addd" -> (match encoding with None | Some S10 -> alu_tiny | Some U27l10 -> alu_tiny_x | Some E27u27l10 -> alu_tiny_y)
+  | s -> fail s
 
-let signed_interval n = begin 
-  assert (n > 0);
-  let min = - pow 2 (n-1)
-  and max = pow 2 (n-1) - 1
-  in (min, max)
-end
+let rec_to_info r : inst_info =
+  let usage = rec_to_usage r
+  in { write_regs = r.write_regs; read_regs = r.read_regs; usage=usage }
 
-let within i interv = match interv with (min, max) -> (i >= min && i <= max)
-
-let signed_length i =
-  let rec f i n = 
-    let interv = signed_interval n
-    in if (within i interv) then n else f i (n+1)
-  in f i 0
-
-let encode_imm imm =
-  let i = Z.to_int imm
-  in let length = signed_length i
-  in if length <= 10 then S10
-  else if length <= 37 then U27l10
-  else if length <= 64 then E27u27l10
-  else failwith @@ sprintf "encode_imm: integer too big! (%d)" i
-
-(** Instruction usages building *)
-
-let arith_rrr_info i rd rs1 rs2 = match i with
-  | Paddl -> { reservation=alu_tiny; write_regs = [rd]; read_regs = [rs1; rs2] }
-  | _ -> failwith "arith_rrr_info: unrecognized constructor"
-
-let arith_rri32_info i rd rs imm32 = match i with
-  | Paddiw -> let restbl = match encode_imm imm32 with S10 -> alu_tiny | U27l10 -> alu_tiny_x | E27u27l10 -> alu_tiny_y in
-      { reservation = restbl; write_regs = [rd]; read_regs = [rs] }
-  | _ -> failwith "arith_rri32_info: unrecognized constructor"
-
-let arith_rri64_info i rd rs imm64 = match i with
-  | Paddil -> let restbl = match encode_imm imm64 with S10 -> alu_tiny | U27l10 -> alu_tiny_x | E27u27l10 -> alu_tiny_y in
-      { reservation = restbl; write_regs = [rd]; read_regs = [rs]}
-  | _ -> failwith "arith_rri64_info: unrecognized constructor"
-
-let arith_info i =
-  match i with
-  | PArithRRI32 (i, rd, rs, imm32) -> arith_rri32_info i rd rs imm32
-  | PArithRRI64 (i, rd, rs, imm64) -> arith_rri64_info i rd rs imm64
-  | PArithRRR (i, rd, rs1, rs2) -> arith_rrr_info i rd rs1 rs2
-  | _ -> failwith "arith_info: unrecognized constructor"
-
-let basic_info i =
-  match i with
-  | PArith i -> arith_info i
-  | _ -> failwith "basic_info: unrecognized constructor"
-
-let control_info i = failwith "control_info: not implemented"
-
-let rec basic_infos body = match body with
-  | [] -> []
-  | bi :: body -> (basic_info bi) :: (basic_infos body)
-
-let exit_info exit = match exit with
-  | None -> []
-  | Some ex -> [control_info ex]
-
-let instruction_infos bb = (basic_infos bb.body) @ (exit_info bb.exit)
+let instruction_infos bb = List.map rec_to_info (instruction_recs bb)
 
 let instruction_usages bb =
-  let usages = List.map (fun info -> info.reservation) (instruction_infos bb)
+  let usages = List.map (fun info -> info.usage) (instruction_infos bb)
   in Array.of_list usages
 
 (** Latency constraints building *)
@@ -138,8 +177,8 @@ let rec get_accesses lregs laccs =
   | [] -> []
   | reg :: lregs -> (accesses reg laccs) @ (get_accesses lregs laccs)
 
-let latency_constraints bb = failwith "latency_constraints: not implemented"
-(* TODO
+let latency_constraints bb =  failwith "latency_constraints: not implemented" 
+  (*
   let written = ref []
   and read = ref []
   and count = ref 0
@@ -155,7 +194,7 @@ let latency_constraints bb = failwith "latency_constraints: not implemented"
     end
   and instr_infos = instruction_infos bb
   in List.iter step instr_infos
-*)
+  *)
 
 (** Dumb schedule if the above doesn't work *)
 
