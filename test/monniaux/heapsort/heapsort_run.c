0fb339fd9dc30b997c76563271e9b3e3f24db84d
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2019-01-19 19:17:46 +0100

    some more example

---
+++ test/monniaux/heapsort/heapsort_run.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "heapsort.h"
+#include "../cycles.h"
+
+int main (void) {
+  cycle_count_config();
+  unsigned len=30000;
+  data *vec = malloc(sizeof(data) * len);
+  data_vec_random(vec, len);
+  cycle_t heapsort_time = get_cycle();
+  heapsort(vec, len);
+  heapsort_time = get_cycle() - heapsort_time;
+  printf("sorted=%s\n"
+	 "heapsort_time=%" PRIu64 "\n",
+	 data_vec_is_sorted(vec, len)?"true":"false",
+	 heapsort_time);
+  free(vec);
+  return 0;
+}
+ 
