6c5df17a302e99dec62aa749ffc30745cc7679a4
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2019-01-19 13:33:57 +0100

    quicksort

---
+++ test/monniaux/cycles.h
@@ -0,0 +1,26 @@
+typedef uint64_t cycle_t;
+
+#ifdef __K1C__
+#include <mppa_bare_runtime/k1c/registers.h>
+static inline void cycle_count_config(void)
+{
+        /* config pmc for cycle count */
+        uint64_t pmc_value = __builtin_k1_get(K1_SFR_PMC);
+
+        pmc_value &= ~(0xfULL);
+        __builtin_k1_set(K1_SFR_PMC, pmc_value);
+}
+
+static inline uint64_t get_cycle(void)
+{
+        return __builtin_k1_get(K1_SFR_PM0);
+}
+#else
+static inline void cycle_count_config(void) { }
+#ifdef  __x86_64__
+#include <x86intrin.h>
+static inline cycle_t get_cycle(void) { return __rdtsc(); }
+#else
+static inline cycle_t get_cycle(void) { return 0; }
+#endif
+#endif
