e5780e9879b31e5847edfdab1b5fb4876e642c6f
Cyril SIX <cyril.six@kalray.eu>
2019-03-19 11:57:59 +0100

    [TEST BROKEN] Trying to replace some int by float in stackhell interop test

--- test/mppa/interop/stackhell.c
+++ test/mppa/interop/stackhell.c
@@ -1,8 +1,9 @@
 #include "framework.h"
 #include "common.h"
 
-BEGIN_TEST(long long)
+BEGIN_TEST(double)
   c = stackhell(a, b, a-b, a+b, a*2, b*2, a*2-b, a+b*2, (a-b)*2, (a+b)*2,
                   -2*a, -2*b, a-b, a+b, a*3, b*3, a*3-b, a+b*3, (a-b)*3, (a+b)*3,
                   -3*a, -3*b, a-b, a+b, a*4, b*4, a*4-b, a+b*4, (a-b)*4, (a+b)*4);
-END_TEST()
+  
+END_TESTF64()
