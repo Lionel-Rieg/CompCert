a0c25ff1259a8373fb71e780f70d28916c321612
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2019-03-20 09:49:45 +0100

    maddl / maddlim are synthesized (but not for pointers it seems)

--- test/monniaux/madd/madd.c
+++ test/monniaux/madd/madd.c
@@ -1,11 +1,25 @@
-unsigned madd(unsigned a, unsigned b, unsigned c) {
+#include <stdint.h>
+
+uint32_t madd(uint32_t a, uint32_t b, uint32_t c) {
   return a + b*c;
 }
 
-unsigned maddimm(unsigned a, unsigned b) {
+uint32_t maddimm(uint32_t a, uint32_t b) {
   return a + b*17113;
 }
 
-unsigned madd2(unsigned a, unsigned b, unsigned c) {
+uint32_t madd2(uint32_t a, uint32_t b, uint32_t c) {
+  return c + b*a;
+}
+
+uint64_t maddl(uint64_t a, uint64_t b, uint64_t c) {
+  return a + b*c;
+}
+
+uint64_t maddlimm(uint64_t a, uint64_t b) {
+  return a + b*17113;
+}
+
+uint64_t maddl2(uint64_t a, uint64_t b, uint64_t c) {
   return c + b*a;
 }
