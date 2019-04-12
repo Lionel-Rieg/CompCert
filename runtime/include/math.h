09db5e13fc268ba5d594d04f767d6e33605eb6a7
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2019-04-12 19:39:55 +0200

    better #include handling

--- runtime/include/math.h
+++ runtime/include/math.h
@@ -1,3 +1,7 @@
-#define isfinite(__y) (fpclassify(__y) >= FP_ZERO)
+#ifndef _COMPCERT_MATH_H
+#define _COMPCERT_MATH_H
+
+#define isfinite(__y) (fpclassify((__y)) >= FP_ZERO)
 
 #include_next <math.h>
+#endif
