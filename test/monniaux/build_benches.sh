ebf6efcadc130ada9b449fad04b19e4a3455a99d
Cyril SIX <cyril.six@kalray.eu>
2019-05-29 17:58:13 +0200

    make -j20 instead of xargs -P4 + make -j5

--- test/monniaux/build_benches.sh
+++ test/monniaux/build_benches.sh
@@ -3,7 +3,8 @@ source benches.sh
 
 rm -f commands.txt
 for bench in $benches; do
-  echo "(cd $bench && make -j5 $1)" >> commands.txt
+  #echo "(cd $bench && make -j5 $1)" >> commands.txt
+  (cd $bench && make -j20 $1)
 done
 
-cat commands.txt | xargs -n1 -I{} -P4 bash -c '{}'
+#cat commands.txt | xargs -n1 -I{} -P4 bash -c '{}'
