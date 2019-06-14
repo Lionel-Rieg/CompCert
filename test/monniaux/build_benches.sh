7e65d55ddfb6b4d44efbd131b989866a3fbe21d9
Cyril SIX <cyril.six@kalray.eu>
2019-06-14 11:32:24 +0200

    build_benches printing compile time in compile_times.txt

--- test/monniaux/build_benches.sh
+++ test/monniaux/build_benches.sh
@@ -1,10 +1,13 @@
+TMPFILE=/tmp/1513times.txt
 
 source benches.sh
 
 rm -f commands.txt
+rm -f $TMPFILE
 for bench in $benches; do
   #echo "(cd $bench && make -j5 $1)" >> commands.txt
-  (cd $bench && make -j20 $1)
+  (cd $bench && make -j20 $1) | grep -P "\d+: \d+\.\d+" >> $TMPFILE
 done
+cat $TMPFILE | sort -n -k 1 > compile_times.txt
 
 #cat commands.txt | xargs -n1 -I{} -P4 bash -c '{}'
