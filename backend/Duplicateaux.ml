bd4fbff2badea3922bf0e144777ae8ecfdc30e74
Cyril SIX <cyril.six@kalray.eu>
2019-10-07 15:09:55 +0200

    Fixing identity PTree in Duplicateaux oracle

--- backend/Duplicateaux.ml
+++ backend/Duplicateaux.ml
@@ -1,7 +1,13 @@
 open RTL
 open Maps
 
+let rec make_identity_ptree_rec = function
+| [] -> PTree.empty
+| m::lm -> let (n, _) = m in PTree.set n n (make_identity_ptree_rec lm)
+
+let make_identity_ptree f = make_identity_ptree_rec (PTree.elements (fn_code f))
+
 (* For now, identity function *)
 let duplicate_aux f =
-  let pTreeEntry = PTree.set (fn_entrypoint f) (fn_entrypoint f) PTree.empty
-  in (((fn_code f), (fn_entrypoint f)), pTreeEntry)
+  let pTreeId = make_identity_ptree f
+  in (((fn_code f), (fn_entrypoint f)), pTreeId)
