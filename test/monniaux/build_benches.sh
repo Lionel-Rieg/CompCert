962c8f9935b257c1df6bddfc88ec41d4822f65c3
Cyril SIX <cyril.six@kalray.eu>
2019-11-13 11:55:18 +0100

    Removing clutter from building + running benches

--- test/monniaux/build_benches.sh
+++ test/monniaux/build_benches.sh
@@ -4,11 +4,16 @@ TMPFILE=/tmp/1513times.txt
 
 source benches.sh
 
+default="\e[39m"
+magenta="\e[35m"
+red="\e[31m"
+
 rm -f commands.txt
 rm -f $TMPFILE
 for bench in $benches; do
+  echo -e "${magenta}Building $bench..${default}"
   if [ "$1" == "" ]; then 
-    (cd $bench && make -j20)
+    (cd $bench && make -s -j20 > /dev/null &> /dev/null) || { echo -e "${red}Build failed" && break; }
   else
     (cd $bench && make -j20) | grep -P "\d+: \d+\.\d+" >> $TMPFILE
   fi
