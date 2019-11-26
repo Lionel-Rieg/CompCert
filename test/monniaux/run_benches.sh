def114623f2d03ea7838900b531f7b3beec5a7a0
Cyril SIX <cyril.six@kalray.eu>
2019-11-26 17:07:59 +0100

    run-benches.sh adapting to number of CPU cores

--- test/monniaux/run_benches.sh
+++ test/monniaux/run_benches.sh
@@ -1,13 +1,16 @@
 
 source benches.sh
 
+cores=$(grep -c ^processor /proc/cpuinfo)
+processes=$((cores/4))
+
 rm -f commands.txt
 for bench in $benches; do
   echo "(cd $bench && echo \"Running $bench..\" &&\
     make -j4 run > /dev/null && echo \"$bench DONE\")" >> commands.txt
 done
 
-cat commands.txt | xargs -n1 -I{} -P6 bash -c '{}'
+cat commands.txt | xargs -n1 -I{} -P$processes bash -c '{}'
 
 ##
 # Gather all the CSV files
