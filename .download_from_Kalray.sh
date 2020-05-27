aa6f270bfc3e9a5ea2c7c02aa98da7fc58342816
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2020-05-27 08:24:50 +0200

    download/untar from Kalray

--- .download_from_Kalray.sh
+++ .download_from_Kalray.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir download
 cd download
-sshpass "-p$KALRAY_SFTP_PASSWORD" sftp -v -o "StrictHostKeyChecking=no" compcert@ssh.kalray.eu <<EOF
+sshpass "-p$KALRAY_SFTP_PASSWORD" sftp -o "StrictHostKeyChecking=no" compcert@ssh.kalray.eu <<EOF
 cd files
 get DEB.tar
 EOF
+tar xf DEB.tar
